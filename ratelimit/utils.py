40ff26efdbf3cd72b36d37f686126e1ecf160260
amit bisht <amit.bisht@knowlarity.com>
2021-07-26 10:21:46 +0530

    added lock if exceed rate limit

--- ratelimit/utils.py
+++ ratelimit/utils.py
@@ -58,7 +58,17 @@ def _method_match(request, method=ALL):
 
 
 rate_re = re.compile(r'([\d]+)/([\d]*)([smhd])?')
+fl_re = re.compile(r'([\d]*)([smhd])?')
 
+
+def _calculate_lock_duration(fl):
+    multi, period = fl_re.match(fl).groups()
+    if not period:
+        period = 's'
+    seconds = _PERIODS[period.lower()]
+    if multi:
+        seconds = seconds * int(multi)
+    return seconds
 
 def _split_rate(rate):
     if isinstance(rate, tuple):
@@ -85,11 +95,15 @@ def _get_window(value, period):
     return w
 
 
-def _make_cache_key(group, rate, value, methods):
+def _make_cache_key(group, rate, value, methods, use_window=True):
     count, period = _split_rate(rate)
     safe_rate = '%d/%ds' % (count, period)
-    window = _get_window(value, period)
-    parts = [group + safe_rate, value, str(window)]
+    if use_window:
+        window = _get_window(value, period)
+        parts = [group + safe_rate, value, str(window)]
+    else:
+        parts = [group + safe_rate, value]
+
     if methods is not None:
         if methods == ALL:
             methods = ''
@@ -100,8 +114,15 @@ def _make_cache_key(group, rate, value, methods):
     return prefix + hashlib.md5(u''.join(parts).encode('utf-8')).hexdigest()
 
 
+
 def is_ratelimited(request, group=None, fn=None, key=None, rate=None,
-                   method=ALL, increment=False):
+                   method=ALL, increment=False, lock_for=None):
+    if lock_for is None:
+        raise ImproperlyConfigured("lock_for duration must be provided")
+
+    lock_for = _calculate_lock_duration(lock_for)
+    # print 'lock duration set to:', lock_for
+
     if group is None:
         if hasattr(fn, '__self__'):
             parts = fn.__module__, fn.__self__.__class__.__name__, fn.__name__
@@ -124,30 +145,59 @@ def is_ratelimited(request, group=None, fn=None, key=None, rate=None,
     if rate is None:
         request.limited = old_limited
         return False
+
+    # get cache object
+    cache_store = getattr(settings, 'RATELIMIT_USE_CACHE', 'default')
+    cache = caches[cache_store]
+
+    # create a cache key for caller
+    value = key_to_value(key, group, request)
+    cache_key = _make_cache_key(group, rate, value, method, use_window=False)
+    # print(cache_key)
+    lock_info = cache.get(cache_key)
+
+    # print 'locked info: ', lock_info
+    ts = time.time()
+    # print'current timestamp: ', ts
+
+    if lock_info:
+        blocked = lock_info['blocked']
+        block_window = lock_info['block_window']
+        # print 'block', block_window
+        if blocked and block_window and ts < block_window:
+            # print 'LOCKED. time left for unlock', block_window - ts
+            return True
+
     usage = get_usage_count(request, group, fn, key, rate, method, increment)
+    # print 'usage: ', usage
 
     fail_open = getattr(settings, 'RATELIMIT_FAIL_OPEN', False)
 
     usage_count = usage.get('count')
+    usage_limit = usage.get('limit')
+
     if usage_count is None:
         limited = not fail_open
     else:
-        usage_limit = usage.get('limit')
         limited = usage_count > usage_limit
 
+    if usage_count > usage_limit:
+        # print 'usage exceeded limit, setting the cache'
+        cache.set(cache_key, {
+            'blocked': True,
+            'block_window': ts + lock_for if lock_for else None
+        })
+    else:
+        # print('none')
+        cache.set(cache_key, None)
+
+    print "="*50
     if increment:
         request.limited = old_limited or limited
     return limited
 
 
-def get_usage_count(request, group=None, fn=None, key=None, rate=None,
-                    method=ALL, increment=False):
-    if not key:
-        raise ImproperlyConfigured('Ratelimit key must be specified')
-    limit, period = _split_rate(rate)
-    cache_name = getattr(settings, 'RATELIMIT_USE_CACHE', 'default')
-    cache = caches[cache_name]
-
+def key_to_value(key, group, request):
     if callable(key):
         value = key(group, request)
     elif key in _SIMPLE_KEYS:
@@ -164,6 +214,17 @@ def get_usage_count(request, group=None, fn=None, key=None, rate=None,
     else:
         raise ImproperlyConfigured(
             'Could not understand ratelimit key: %s' % key)
+    return value
+
+def get_usage_count(request, group=None, fn=None, key=None, rate=None,
+                    method=ALL, increment=False):
+    if not key:
+        raise ImproperlyConfigured('Ratelimit key must be specified')
+    limit, period = _split_rate(rate)
+    cache_name = getattr(settings, 'RATELIMIT_USE_CACHE', 'default')
+    cache = caches[cache_name]
+
+    value = key_to_value(key, group, request)
 
     cache_key = _make_cache_key(group, rate, value, method)
     time_left = _get_window(value, period) - int(time.time())
